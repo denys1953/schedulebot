7f81719f01420afd843233b0be252110e6e601a5
Your Name <den257840@gmail.com>
2022-03-09 21:04:25 +0200

    make it better

--- main.py
+++ main.py
@@ -112,9 +112,9 @@ def main():
             while i < int(message.text):
                 try:
                     film_info = get_film()
-                    bot.send_message(message.chat.id, film_info)
-                    if film_info["ratingKinopoisk"] != None and film_info["ratingKinopoisk"] > 6:
-                        if film_info["ratingIMDB"] != None and film_info["ratingIMDB"] > 6:
+
+                    if film_info["ratingKinopoisk"] != None and film_info["ratingKinopoisk"] > 6.5:
+                        if film_info["ratingImdb"] != None and film_info["ratingImdb"] > 6.5:
                             if film_info["year"] > 1990:
                                 if film_info["serial"] == False and film_info["shortFilm"] == False and film_info["has3D"] == False:
                                     genres = []
